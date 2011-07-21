44d8385122b227a97e11f847ee9ef2d1414c4396
Jan Valentik <janvalentik@janvalentik.cz>
2011-07-21 09:45:43 +0200

    Aktualizace captcha

--- remote/cimage.php
+++ remote/cimage.php
@@ -3,60 +3,131 @@
 //jadro
 define('_indexroot', '../');
 define('_tmp_customheader', '');
-require(_indexroot."core.php");
+require (_indexroot . "core.php");
 
 //kontrola GD
 _checkGD("jpg");
 
 //inicializace obrazku, nacteni kodu
-$imgw=65; $imgh=22;
-$img=imagecreate($imgw, $imgh);
-if(isset($_GET['n']) and isset($_SESSION[_sessionprefix.'captcha_code'][$_GET['n']])){$code=$_SESSION[_sessionprefix.'captcha_code'][$_GET['n']];}
-else{$code='';}
+$imgw = 65;
+$imgh = 22;
+$img = imagecreate($imgw, $imgh);
+if (isset($_GET['n']) and isset($_SESSION[_sessionprefix . 'captcha_code'][(int) $_GET['n']])) {
+    list($code, $drawn) = $_SESSION[_sessionprefix . 'captcha_code'][(int) $_GET['n']];
+    if ($drawn)
+	die;
+    $_SESSION[_sessionprefix . 'captcha_code'][(int) $_GET['n']][1] = true;
+} else
+    die;
 
-//zavedeni barev
-$bg=imagecolorallocate($img, 255, 255, 255);
-$dark=imagecolorallocate($img, 000, 000, 000);
-$normal=imagecolorallocate($img, 200, 200, 200);
-$bright=imagecolorallocate($img, 225, 225, 225);
+/**
+ * Linear perspective class
+ */
+class linear_perspective {
 
-//normalni tecky
-$pocet=0;
-while($pocet<=0){
-imagesetpixel($img, mt_rand(0, $imgw-2), mt_rand(0, $imgh), $normal);
-$pocet++;
+    public $cam_location = array('x' => -30, 'y' => 0, 'z' => -250);
+    public $cam_rotation = array('x' => -1, 'y' => 0, 'z' => 0);
+    public $viewer_position = array('x' => 450, 'y' => -500, 'z' => -80);
+
+    public function getProjection($point) {
+
+	$translation = array();
+	$projection = array();
+
+	$translation['x'] = cos($this->cam_rotation['y']) * (sin($this->cam_rotation['z']) * ($point['y'] - $this->cam_location['y']) + cos($this->cam_rotation['z']) * ($point['x'] - $this->cam_location['x'])) - sin($this->cam_rotation['y']) * ($point['z'] - $this->cam_location['z']);
+	$translation['y'] = sin($this->cam_rotation['x']) * (cos($this->cam_rotation['y']) * ($point['z'] - $this->cam_location['z']) + sin($this->cam_rotation['y']) * (sin($this->cam_rotation['z']) * ($point['y'] - $this->cam_location['y']) + cos($this->cam_rotation['z']) * ($point['x'] - $this->cam_location['x']))) + cos($this->cam_rotation['z']) * (cos($this->cam_rotation['z']) * ($point['y'] - $this->cam_location['y']) - sin($this->cam_rotation['z']) * ($point['x'] - $this->cam_location['x']));
+	$translation['z'] = cos($this->cam_rotation['x']) * (cos($this->cam_rotation['y']) * ($point['z'] - $this->cam_location['z']) + sin($this->cam_rotation['y']) * (sin($this->cam_rotation['z']) * ($point['y'] - $this->cam_location['y']) + cos($this->cam_rotation['z']) * ($point['x'] - $this->cam_location['x']))) - sin($this->cam_rotation['z']) * (cos($this->cam_rotation['z']) * ($point['y'] - $this->cam_location['y']) - sin($this->cam_rotation['z']) * ($point['x'] - $this->cam_location['x']));
+
+	$projection['x'] = ($translation['x'] - $this->viewer_position['x']) * ($this->viewer_position['z'] / $translation['z']);
+	$projection['y'] = ($translation['y'] - $this->viewer_position['y']) * ($this->viewer_position['z'] / $translation['z']);
+
+	return $projection;
+    }
+
 }
 
-//svetle tecky
-$pocet=0;
-while($pocet<=0){
-imagesetpixel($img, mt_rand(0, $imgw), mt_rand(0, $imgh), $bright);
-$pocet++;
+function imagelightnessat($img, $x, $y) {
+
+    if (!is_resource($img))
+	return 0.0;
+
+    $c = @imagecolorat($img, $x, $y);
+    if ($c === false)
+	return false;
+
+    if (imageistruecolor($img)) {
+	$red = ($c >> 16) & 0xFF;
+	$green = ($c >> 8) & 0xFF;
+	$blue = $c & 0xFF;
+    } else {
+	$i = imagecolorsforindex($img, $c);
+	$red = $i['red'];
+	$green = $i['green'];
+	$blue = $i['blue'];
+    }
+
+    $m = min($red, $green, $blue);
+    $n = max($red, $green, $blue);
+    $lightness = (double) (($m + $n) / 510.0);
+    return ($lightness);
 }
 
-//temne tecky
-$pocet=0;
-while($pocet<=0){
-imagesetpixel($img, mt_rand(0, $imgw), mt_rand(0, $imgh), $dark);
-$pocet++;
+$perspective = new linear_perspective();
+
+// sizes and offsets
+$matrix_dim = array('x' => 85, 'y' => 30);
+$captcha_dim = array('x' => 410, 'y' => 120);
+$distance = array('x' => 1, 'y' => 1, 'z' => 1);
+$metric = array('x' => 10, 'y' => 25, 'z' => 5);
+$offset = array('x' => 198, 'y' => -60);
+
+// matrix
+$matrix = imagecreatetruecolor($matrix_dim['x'], $matrix_dim['y']);
+$black = imagecolorexact($matrix, 0, 0, 0);
+$white = imagecolorexact($matrix, 255, 255, 255);
+$gray = imagecolorexact($matrix, 200, 200, 200);
+imagefill($matrix, 0, 0, $white);
+
+// random pixels
+for ($i = 0; $i < 300; ++$i)
+    imagesetpixel($matrix, mt_rand(5, ($matrix_dim['x'] - 6)), mt_rand(5, ($matrix_dim['y'] - 6)), $gray);
+
+// text
+imagefttext($matrix, 19, 0, 4, 25, $black, 'cimage.ttf', $code);
+
+// rotate
+$matrix = imagerotate($matrix, mt_rand(-1, 1), $white);
+
+// compute 3D points
+$point = array();
+for ($x = 0; $x < $matrix_dim['x']; $x++) {
+    for ($y = 0; $y < $matrix_dim['y']; $y++) {
+	$lightness = imagelightnessat($matrix, $x, $y);
+	$point[$x][$y] = $perspective->getProjection(array('x' => $x * $metric['x'] + $distance['x'], 'y' => $lightness * $metric['y'] + $distance['y'], 'z' => ($matrix_dim['y'] - $y) * $metric['z'] + $distance['z']));
+    }
 }
+imagedestroy($matrix);
 
-//cary
-$pocet=0;
-while($pocet<=0){
-imageline($img, mt_rand(0, $imgw), mt_rand(0, $imgh), mt_rand(0, $imgw), mt_rand(0, $imgh), $normal);
-$pocet++;
-}
+// captcha image
+$captcha = imagecreatetruecolor($captcha_dim['x'], $captcha_dim['y']);
+$black = imagecolorexact($captcha, 0, 0, 0);
+$white = imagecolorexact($captcha, 255, 255, 255);
+imagefill($captcha, 0, 0, $white);
 
-//ramecek
-imagerectangle($img, 0, 0, $imgw-1, $imgh-1, $normal);
+// draw countour lines
+imageantialias($captcha, true);
+for ($x = 1; $x < $matrix_dim['x']; $x++)
+    for ($y = 1; $y < $matrix_dim['y']; $y++)
+	imageline($captcha, -$point[$x - 1][$y - 1]['x'] + $offset['x'], -$point[$x - 1][$y - 1]['y'] + $offset['y'], -$point[$x][$y]['x'] + $offset['x'], -$point[$x][$y]['y'] + $offset['y'], $black);
 
-//text
-imagestring($img, 5, 10, 3, $code, $dark);
-
-//odeslani obrazku
-header("Content-type: image/JPEG");
-imagejpeg($img, null, 100);
-imagedestroy($img);
-
+// output
+//imagepng($captcha); die;
+//imagefilledrectangle($captcha, 0, 0, 90, 25, $white);
+//imagefttext($captcha, 20, 0, 8, 20, $black, k::classPath('iCaptcha').k::DATA_DIRNAME.'/font.ttf', $code);
+$width = 160;
+$height = floor($width / ($captcha_dim['x'] / $captcha_dim['y']));
+$rcaptcha = imagecreatetruecolor($width, $height);
+imagecopyresampled($rcaptcha, $captcha, 0, 0, 0, 0, $width, $height, $captcha_dim['x'], $captcha_dim['y']);
+header('Content-Type: image/png');
+imagepng($rcaptcha);
 ?>
--- remote/post.php
+++ remote/post.php
@@ -1,194 +1,200 @@
 <?php
-
-/*--- incializace jadra ---*/
+/* --- incializace jadra --- */
 define('_indexroot', '../');
-require(_indexroot."core.php");
+require(_indexroot . "core.php");
 
-/*--- zpracovani ---*/
+/* --- zpracovani --- */
 
-  /*-- nacteni promennych --*/
-  
-    //kontrola zvoleni
-    _checkKeys('_POST', array('_posttarget', '_posttype', 'text'));
-    _checkKeys('_GET', array('_return'));
-  
-    //jmeno hosta nebo ID uzivatele
-    if(_loginindicator){
-      $guest="";
-      $author=_loginid;
-    }
-    else{
-      if(isset($_POST['guest'])){
-        $guest=$_POST['guest'];
-        if(mb_strlen($guest)>24){$guest=mb_substr($guest, 0, 24);}
-        $guest=_anchorStr($guest, false);
-      }
-      else{
-        $guest="";
-      }
-      
-      $author=-1;
-    }
-    
-    //typ, domov, text
-    $posttarget=intval($_POST['_posttarget']);
-    $posttype=intval($_POST['_posttype']);
-    $text=_safeStr(_htmlStr(_wsTrim(_cutStr($_POST['text'], (($posttype!=4)?3072:255), false))));
+/* -- nacteni promennych -- */
 
-    //domovsky prispevek
-    if($posttype!=4){
-      _checkKeys('_POST', array('_xhome'));
-      $xhome=intval($_POST['_xhome']);
-    }
-    else{
-      $xhome=-1;
+//kontrola zvoleni
+_checkKeys('_POST', array('_posttarget', '_posttype', 'text'));
+_checkKeys('_GET', array('_return'));
+
+//jmeno hosta nebo ID uzivatele
+if (_loginindicator) {
+    $guest = "";
+    $author = _loginid;
+} else {
+    if (isset($_POST['guest'])) {
+	$guest = $_POST['guest'];
+	if (mb_strlen($guest) > 24) {
+	    $guest = mb_substr($guest, 0, 24);
+	}
+	$guest = _anchorStr($guest, false);
+    } else {
+	$guest = "";
     }
 
-    //predmet
-    if($xhome==-1 and $posttype!=4){
-      _checkKeys('_POST', array('subject'));
-      $subject=_safeStr(_htmlStr(_wsTrim(_cutStr($_POST['subject'], 22, false))));
-    }
-    else{
-      $subject="";
-    }
+    $author = -1;
+}
 
-    //vyplneni prazdnych poli
-    if($subject=="" and $xhome==-1 and $posttype!=4){$subject="-";}
-    if($guest=="" and !_loginindicator){$guest=$_lang['posts.anonym'];}
-    
-    //typ 6 je zpracovanim stejny jako 5
-    if($posttype==6){$posttype=5;}
+//typ, domov, text
+$posttarget = intval($_POST['_posttarget']);
+$posttype = intval($_POST['_posttype']);
+$text = _safeStr(_htmlStr(_wsTrim(_cutStr($_POST['text'], (($posttype != 4) ? 3072 : 255), false))));
+
+//domovsky prispevek
+if ($posttype != 4) {
+    _checkKeys('_POST', array('_xhome'));
+    $xhome = intval($_POST['_xhome']);
+} else {
+    $xhome = -1;
+}
+
+//predmet
+if ($xhome == -1 and $posttype != 4) {
+    _checkKeys('_POST', array('subject'));
+    $subject = _safeStr(_htmlStr(_wsTrim(_cutStr($_POST['subject'], 22, false))));
+} else {
+    $subject = "";
+}
+
+//vyplneni prazdnych poli
+if ($subject == "" and $xhome == -1 and $posttype != 4) {
+    $subject = "-";
+}
+if ($guest == "" and !_loginindicator) {
+    $guest = $_lang['posts.anonym'];
+}
+
+//typ 6 je zpracovanim stejny jako 5
+if ($posttype == 6) {
+    $posttype = 5;
+}
 
 
-  /*-- kontrola cile --*/
-  $continue=false;
-  switch($posttype){
+/* -- kontrola cile -- */
+$continue = false;
+switch ($posttype) {
 
     //sekce
     case 1:
-    $tdata=mysql_query("SELECT public,var1,var3 FROM `"._mysql_prefix."-root` WHERE id=".$posttarget." AND type=1");
-      if(mysql_num_rows($tdata)!=0){
-      $tdata=mysql_fetch_array($tdata);
-        if(_publicAccess($tdata['public']) and $tdata['var1']==1 and $tdata['var3']!=1){
-          $continue=true;
-        }
-      }
-    break;
+	$tdata = mysql_query("SELECT public,var1,var3 FROM `" . _mysql_prefix . "-root` WHERE id=" . $posttarget . " AND type=1");
+	if (mysql_num_rows($tdata) != 0) {
+	    $tdata = mysql_fetch_array($tdata);
+	    if (_publicAccess($tdata['public']) and $tdata['var1'] == 1 and $tdata['var3'] != 1) {
+		$continue = true;
+	    }
+	}
+	break;
 
     //clanek
     case 2:
-    $tdata=mysql_query("SELECT id,time,confirmed,public,home1,home2,home3,comments,commentslocked FROM `"._mysql_prefix."-articles` WHERE id=".$posttarget);
-      if(mysql_num_rows($tdata)!=0){
-      $tdata=mysql_fetch_array($tdata);
-        if(_articleAccess($tdata)==1 and $tdata['comments']==1 and $tdata['commentslocked']==0){
-          $continue=true;
-        }
-      }
-    break;
+	$tdata = mysql_query("SELECT id,time,confirmed,public,home1,home2,home3,comments,commentslocked FROM `" . _mysql_prefix . "-articles` WHERE id=" . $posttarget);
+	if (mysql_num_rows($tdata) != 0) {
+	    $tdata = mysql_fetch_array($tdata);
+	    if (_articleAccess($tdata) == 1 and $tdata['comments'] == 1 and $tdata['commentslocked'] == 0) {
+		$continue = true;
+	    }
+	}
+	break;
 
     //kniha
     case 3:
-    $tdata=mysql_query("SELECT public,var1,var3 FROM `"._mysql_prefix."-root` WHERE id=".$posttarget." AND type=3");
-      if(mysql_num_rows($tdata)!=0){
-      $tdata=mysql_fetch_array($tdata);
-        if(_publicAccess($tdata['public']) and _publicAccess($tdata['var1']) and $tdata['var3']!=1){
-          $continue=true;
-        }
-      }
+	$tdata = mysql_query("SELECT public,var1,var3 FROM `" . _mysql_prefix . "-root` WHERE id=" . $posttarget . " AND type=3");
+	if (mysql_num_rows($tdata) != 0) {
+	    $tdata = mysql_fetch_array($tdata);
+	    if (_publicAccess($tdata['public']) and _publicAccess($tdata['var1']) and $tdata['var3'] != 1) {
+		$continue = true;
+	    }
+	}
 
-    break;
+	break;
 
     //shoutbox
     case 4:
-    $tdata=mysql_query("SELECT public,locked FROM `"._mysql_prefix."-sboxes` WHERE id=".$posttarget);
-      if(mysql_num_rows($tdata)!=0){
-      $tdata=mysql_fetch_array($tdata);
-        if(_publicAccess($tdata['public']) and $tdata['locked']!=1){
-          $continue=true;
-        }
-      }
-    break;
+	$tdata = mysql_query("SELECT public,locked FROM `" . _mysql_prefix . "-sboxes` WHERE id=" . $posttarget);
+	if (mysql_num_rows($tdata) != 0) {
+	    $tdata = mysql_fetch_array($tdata);
+	    if (_publicAccess($tdata['public']) and $tdata['locked'] != 1) {
+		$continue = true;
+	    }
+	}
+	break;
 
     //forum
     case 5:
-    $tdata=mysql_query("SELECT public,var2,var3 FROM `"._mysql_prefix."-root` WHERE id=".$posttarget." AND type=8");
-      if(mysql_num_rows($tdata)!=0){
-      $tdata=mysql_fetch_array($tdata);
-        if(_publicAccess($tdata['public']) and _publicAccess($tdata['var3']) and $tdata['var2']!=1){
-          $continue=true;
-        }
-      }
+	$tdata = mysql_query("SELECT public,var2,var3 FROM `" . _mysql_prefix . "-root` WHERE id=" . $posttarget . " AND type=8");
+	if (mysql_num_rows($tdata) != 0) {
+	    $tdata = mysql_fetch_array($tdata);
+	    if (_publicAccess($tdata['public']) and _publicAccess($tdata['var3']) and $tdata['var2'] != 1) {
+		$continue = true;
+	    }
+	}
 
-    break;
+	break;
 
     //galerie
     case 7:
-    $tdata=mysql_query("SELECT public,var4,var5 FROM `"._mysql_prefix."-root` WHERE id=".$posttarget." AND type=5");
-      if(mysql_num_rows($tdata)!=0){
-      $tdata=mysql_fetch_array($tdata);
-        if(_publicAccess($tdata['public']) and $tdata['var4']==1 and $tdata['var5']!=1){
-          $continue=true;
-        }
-      }
-    break;
+	$tdata = mysql_query("SELECT public,var4,var5 FROM `" . _mysql_prefix . "-root` WHERE id=" . $posttarget . " AND type=5");
+	if (mysql_num_rows($tdata) != 0) {
+	    $tdata = mysql_fetch_array($tdata);
+	    if (_publicAccess($tdata['public']) and $tdata['var4'] == 1 and $tdata['var5'] != 1) {
+		$continue = true;
+	    }
+	}
+	break;
+}
 
-  }
-  
-  /*-- kontrola prispevku pro odpoved --*/
-  if($xhome!=-1){
-    $continue2=false;
-    $tdata=mysql_query("SELECT xhome FROM `"._mysql_prefix."-posts` WHERE id=".$xhome." AND home=".$posttarget);
-      if(mysql_num_rows($tdata)!=0){
-        $tdata=mysql_fetch_array($tdata);
-        if($tdata['xhome']==-1){$continue2=true;}
-      }
-  }
-  else{
-    $continue2=true;
-  }
-  
-  /*-- ulozeni prispevku --*/
-  if($continue and $continue2 and $text!="" and _captchaCheck()){
-    if($posttype==4 or _iplogCheck(5)){
-      $newid=_getNewID("posts");
-      mysql_query("INSERT INTO `"._mysql_prefix."-posts` (id,type,home,xhome,subject,text,author,guest,time,ip,bumptime) VALUES (".$newid.",".$posttype.",".$posttarget.",".$xhome.",'".$subject."','".$text."',".$author.",'".$guest."',".time().",'"._userip."',".(($posttype == 5 && $xhome == -1) ? 'UNIX_TIMESTAMP()' : '0').")");
-      if(!_loginright_unlimitedpostaccess and $posttype!=4){_iplogUpdate(5);}
-      $return=1;
-
-        // topicy - aktualizace bumptime
-        if($posttype == 5 && $xhome != -1) {
-            mysql_query("UPDATE `"._mysql_prefix."-posts` SET bumptime=UNIX_TIMESTAMP() WHERE id=".$xhome);
-        }
-
-        //shoutboxy - odstraneni prispevku za hranici limitu
-        if($posttype==4){
-          $pnum=mysql_result(mysql_query("SELECT COUNT(id) FROM `"._mysql_prefix."-posts` WHERE type=4 AND home=".$posttarget), 0);
-          if($pnum>_sboxmemory){
-            $dnum=$pnum-_sboxmemory;
-            $dposts=mysql_query("SELECT id FROM `"._mysql_prefix."-posts` WHERE type=4 AND home=".$posttarget." ORDER BY id LIMIT ".$dnum);
-              while($dpost=mysql_fetch_array($dposts)){
-                mysql_query("DELETE FROM `"._mysql_prefix."-posts` WHERE id=".$dpost['id']);
-              }
-          }
-        }
-      
+/* -- kontrola prispevku pro odpoved -- */
+if ($xhome != -1) {
+    $continue2 = false;
+    $tdata = mysql_query("SELECT xhome FROM `" . _mysql_prefix . "-posts` WHERE id=" . $xhome . " AND home=" . $posttarget);
+    if (mysql_num_rows($tdata) != 0) {
+	$tdata = mysql_fetch_array($tdata);
+	if ($tdata['xhome'] == -1) {
+	    $continue2 = true;
+	}
     }
-    else{
-      $return=2;
-    }
-  }
-  else{
-    $return=0;
-  }
+} else {
+    $continue2 = true;
+}
 
-/*--- presmerovani ---*/
-if($posttype!=4){
-  if(!isset($_POST['subject'])){$_POST['subject']="";}
-  if($return!=1){$returnurl=_addFdGetToLink(_addGetToLink($_GET['_return'], "replyto=".$xhome, false), array("guest"=>$guest, "subject"=>$_POST['subject'], "text"=>$_POST['text']))."&addpost";}
-  else{$returnurl=$_GET['_return'];}
-  $_GET['_return']=_addGetToLink($returnurl, "r=".$return."#posts", false);
+/* -- ulozeni prispevku -- */
+if ($continue and $continue2 and $text != "" and ($posttype == 4 || _captchaCheck())) {
+    if ($posttype == 4 or _iplogCheck(5)) {
+	$newid = _getNewID("posts");
+	mysql_query("INSERT INTO `" . _mysql_prefix . "-posts` (id,type,home,xhome,subject,text,author,guest,time,ip,bumptime) VALUES (" . $newid . "," . $posttype . "," . $posttarget . "," . $xhome . ",'" . $subject . "','" . $text . "'," . $author . ",'" . $guest . "'," . time() . ",'" . _userip . "'," . (($posttype == 5 && $xhome == -1) ? 'UNIX_TIMESTAMP()' : '0') . ")");
+	if (!_loginright_unlimitedpostaccess and $posttype != 4) {
+	    _iplogUpdate(5);
+	}
+	$return = 1;
+
+	// topicy - aktualizace bumptime
+	if ($posttype == 5 && $xhome != -1) {
+	    mysql_query("UPDATE `" . _mysql_prefix . "-posts` SET bumptime=UNIX_TIMESTAMP() WHERE id=" . $xhome);
+	}
+
+	//shoutboxy - odstraneni prispevku za hranici limitu
+	if ($posttype == 4) {
+	    $pnum = mysql_result(mysql_query("SELECT COUNT(id) FROM `" . _mysql_prefix . "-posts` WHERE type=4 AND home=" . $posttarget), 0);
+	    if ($pnum > _sboxmemory) {
+		$dnum = $pnum - _sboxmemory;
+		$dposts = mysql_query("SELECT id FROM `" . _mysql_prefix . "-posts` WHERE type=4 AND home=" . $posttarget . " ORDER BY id LIMIT " . $dnum);
+		while ($dpost = mysql_fetch_array($dposts)) {
+		    mysql_query("DELETE FROM `" . _mysql_prefix . "-posts` WHERE id=" . $dpost['id']);
+		}
+	    }
+	}
+    } else {
+	$return = 2;
+    }
+} else {
+    $return = 0;
+}
+
+/* --- presmerovani --- */
+if ($posttype != 4) {
+    if (!isset($_POST['subject'])) {
+	$_POST['subject'] = "";
+    }
+    if ($return != 1) {
+	$returnurl = _addFdGetToLink(_addGetToLink($_GET['_return'], "replyto=" . $xhome, false), array("guest" => $guest, "subject" => $_POST['subject'], "text" => $_POST['text'])) . "&addpost";
+    } else {
+	$returnurl = $_GET['_return'];
+    }
+    $_GET['_return'] = _addGetToLink($returnurl, "r=" . $return . "#posts", false);
 }
 _returnHeader();
-
 ?>
